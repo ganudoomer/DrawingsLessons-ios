96d0b3dc194111981a7059eb21bc5138abe46b89
ganudoomer <ganuiscool@gmail.com>
2021-10-05 19:42:57 +0530

    Initial Commit

---
+++ DrawingsDemo/DrawingsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingsDemoApp.swift
+//  DrawingsDemo
+//
+//  Created by Sree on 05/10/21.
+//
+
+import SwiftUI
+
+@main
+struct DrawingsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
