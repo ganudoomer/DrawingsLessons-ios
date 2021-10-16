1ff74f31efcb3a28c2db3c5c4a79b7378a97b8af
ganudoomer <ganuiscool@gmail.com>
2021-10-16 19:56:18 +0530

    Changed circle shape into square

--- DrawingsDemo/ContentView.swift
+++ DrawingsDemo/ContentView.swift
@@ -52,11 +52,110 @@ struct ContentView: View {
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+
+struct Flower: Shape {
+    var petalOffset: Double = -20
+    var petalWidth: Double = 100
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        for number in stride(from: 0,to: CGFloat.pi * 2,by: CGFloat.pi/8){
+            let rotation = CGAffineTransform(rotationAngle: number)
+            
+    
+            let postion = rotation.concatenating(CGAffineTransform(translationX: rect.width/2, y: rect.height/2))
+            
+            let originalPetal = Path(ellipseIn:CGRect(x:CGFloat(petalOffset),y:0,width: CGFloat(petalWidth),height:rect.width/2))
+            let roatedPetal = originalPetal.applying(postion)
+            
+            path.addPath(roatedPetal)
+        }
+        return path
     }
 }
+
+
+// Making a flower
+struct  ContentViewNew: View {
+    @State private var petalOffset = -20.0
+    @State private var petalWidth  = 100.0
+    var body: some View {
+        VStack{
+            Flower(petalOffset: Double(petalOffset), petalWidth: petalWidth).fill(Color.red,style: FillStyle(eoFill:true))
+
+            Text("Offset")
+            Slider(value: $petalOffset,in:-40...40).padding([.horizontal,.bottom])
+            
+            Text("Width")
+            Slider(value: $petalWidth,in: -40...40).padding([.horizontal,.bottom])
+            
+            
+        }
+    }
+}
+
+
+ // Border images
+// Image Paint
+
+struct Lesson2 : View {
+    var body: some View {
+        Capsule().strokeBorder(ImagePaint(image: Image("Example"),scale: 0.1), lineWidth: 20).frame(width: 300, height: 200)
+        
+        Text("Hello World").frame(width: 300, height: 300).border(ImagePaint(image: Image("Example"),sourceRect:CGRect(x:0,y:0.25, width: 1.0,height:0.5),scale: 0.1),width: 30)
+    }
+}
+
+
+struct ColorCycleCricle: View {
+    var amount = 0.0
+    var steps = 100
+    var body: some View {
+        ZStack{
+            ForEach(0..<steps){value in
+                Rectangle().inset(by: CGFloat(value))
+                    .strokeBorder(LinearGradient(gradient: Gradient(colors: [
+                        self.color(for: value, brightness: 1),
+                        self.color(for: value, brightness: 0.5)
+                    ]), startPoint: .top, endPoint: .bottom),lineWidth:2)
+            }
+            // Add when Metal GPU
+        }.drawingGroup()
+    }
+    
+    func color(for value:Int,brightness: Double) -> Color {
+        var targetHue = Double(value) / Double(self.steps) + self.amount
+        if targetHue > 1 {
+            targetHue -= 1
+        }
+        return Color(hue:targetHue,saturation: 1,brightness: brightness)
+    }
+
+
+}
+
+
+struct Lesson3 : View {
+    @State private var colorCycle = 0.0
+    var body: some View {
+        VStack{
+            ColorCycleCricle(amount: self.colorCycle).frame(width: 300, height: 300)
+            Slider(value: $colorCycle)
+        }
+    }
+}
+
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Lesson3()
+    }
+}
+
+
+
 
 
 
